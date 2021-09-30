64be7d3c7da4130eb59fe493a20598213bfcef41
efojug <lidudu890890@163.com>
2021-09-30 16:47:01 +0800

    2021/9/30

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,17 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using ProjBobcat.DefaultComponent.Launch;
 using System.Windows;
 
 namespace MCLauncher
 {
+    
     /// <summary>
     /// App.xaml 的交互逻辑
     /// </summary>
     public partial class App : Application
     {
+        public static DefaultGameCore core;
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -9,33 +9,58 @@ using Panuon.UI.Silver;
 using KMCCC.Launcher;
 using KMCCC.Authentication;
 using System.IO;
-using System.Text;
+using ProjBobcat.DefaultComponent.Launch;
 
 namespace MCLauncher
 {
-    /// <summary>
-    /// MainWindow.xaml 的交互逻辑
-    /// </summary>
     public partial class MainWindow : WindowX
     {
+        public static void InitLauncherCore()
+        {
+            var clientToken = new Guid("88888888-8888-8888-8888-888888888888");
+            var rootPath = ".minecraft";
+            core = new DefaultGameCore
+            {
+                ClientToken = clientToken,
+                RootPath = rootPath,
+                VersionLocator = new DefaultVersionLocator(rootPath, clientToken)
+                {
+                    LauncherProfileParser = new DefaultLauncherProfileParser(rootPath, clientToken)
+                }
+            };
+        }
         LoginUI.Offline Offline = new LoginUI.Offline();
         LoginUI.Online Online = new LoginUI.Online();
         LoginUI.Microsoft Microsoft = new LoginUI.Microsoft();
         public int launchMode = 1;
+        public static DefaultGameCore core;
         SquareMinecraftLauncher.Minecraft.Tools tools = new SquareMinecraftLauncher.Minecraft.Tools();
         public static LauncherCore Core = LauncherCore.Create();
         public MainWindow()
         {
+            InitializeComponent();
+            List<string> javaList = new List<string>();
+            List<string> memoryList = new List<string>();
+            int memorynum = 512;
+            for(int i = 0; i < 7; i++)
+            {
+                memoryList.Add(Convert.ToString(memorynum) + "M");
+                memorynum *= 2;
+            }
+            memoryCombo.ItemsSource = memoryList;
+            memoryCombo.SelectedItem = memoryCombo.Items[2];
             string path = @"emcl.config";
             if (File.Exists(path))
             {
                 try
                 {
                     string[] contents = File.ReadAllLines(path);
+                    memoryCombo.SelectedItem = contents[0];
+                    versionCombo.SelectedItem = contents[1];
                 }
-                catch (Exception ex) 
+                catch (Exception ex)
                 {
-                    MessageBox.Show(ex.Message, "错误");
+                    MessageBoxX.Show(ex.Message, "错误");
                 }
             }
             else
@@ -47,15 +72,14 @@ namespace MCLauncher
                 }
                 catch (Exception ex)
                 {
-                    MessageBox.Show(ex.Message, "错误");
+                    MessageBoxX.Show(ex.Message, "错误");
                 }
             }
-
-            InitializeComponent();
-            var versions = Core.GetVersions().ToArray();
-            versionCombo.ItemsSource = versions;
-            List<string> javaList = new List<string>();;
             foreach (string i in KMCCC.Tools.SystemTools.FindJava())
+            {
+                javaList.Add(i);
+            }
+            foreach (string i in ProjBobcat.Class.Helper.SystemInfoHelper.FindJava())
             {
                 javaList.Add(i);
             }
@@ -80,6 +104,20 @@ namespace MCLauncher
         }
         public async void GameStart()
         {
+            try
+            {
+                var ver = (KMCCC.Launcher.Version)versionCombo.SelectedItem;
+                using (StreamWriter config = new StreamWriter("emcl.config"))
+                {
+                    config.WriteLine(memoryCombo.SelectedItem);
+                    config.WriteLine(ver);
+                    config.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBoxX.Show(ex.Message, "错误");
+            }
             LaunchOptions launchOptions = new LaunchOptions();
             switch (launchMode)
             {
@@ -90,8 +128,8 @@ namespace MCLauncher
                     launchOptions.Authenticator = new YggdrasilLogin(Online.Email.Text,Online.Password.Password, false);
                     break;
             }
-            launchOptions.MaxMemory = Convert.ToInt32(MemoryTextbox.Text);
-            if (versionCombo.Text != string.Empty && javaCombo.Text != string.Empty && (Offline.IDText.Text != string.Empty || (Online.Email.Text != string.Empty && Online.Password.Password != string.Empty) && MemoryTextbox.Text != string.Empty))
+            launchOptions.MaxMemory = Convert.ToInt32(memoryCombo.SelectedItem);
+            if (versionCombo.Text != string.Empty && javaCombo.Text != string.Empty && (Offline.IDText.Text != string.Empty || (Online.Email.Text != string.Empty && Online.Password.Password != string.Empty) && memoryCombo.Text != string.Empty))
             {
                 try
                 {
@@ -101,10 +139,6 @@ namespace MCLauncher
                         var ver = (KMCCC.Launcher.Version)versionCombo.SelectedItem;
                         launchOptions.Version = ver;
                         var result = Core.Launch(launchOptions);
-                        if (result.Success)
-                        {
-                            MessageBoxX.Show("游戏启动成功，可能会有10-20秒的延迟，请耐心等待", "提示");
-                        }
                         if (!result.Success)
                         {
                             switch (result.ErrorType)
@@ -126,15 +160,22 @@ namespace MCLauncher
                     }
                     else
                     {
-                        microsoft_launcher.MicrosoftAPIs microsoftAPIs = new microsoft_launcher.MicrosoftAPIs();
-                        var v = Microsoft.wb.Source.ToString().Replace(microsoftAPIs.cutUri, string.Empty);
-                        var t = Task.Run(() => {
-                            return microsoftAPIs.GetAccessTokenAsync(v, false).Result;
-                        });
-                        await t;
-                        var v1 = microsoftAPIs.GetAllThings(t.Result.access_token, false);
-                        SquareMinecraftLauncher.Minecraft.Game game = new SquareMinecraftLauncher.Minecraft.Game();
-                        await game.StartGame(versionCombo.Text, javaCombo.Text, Convert.ToInt32(MemoryTextbox.Text), v1.name, v1.uuid, v1.mcToken, string.Empty, string.Empty);
+                        try
+                        {
+                            microsoft_launcher.MicrosoftAPIs microsoftAPIs = new microsoft_launcher.MicrosoftAPIs();
+                            var v = Microsoft.wb.Source.ToString().Replace(microsoftAPIs.cutUri, string.Empty);
+                            var t = Task.Run(() => {
+                                return microsoftAPIs.GetAccessTokenAsync(v, false).Result;
+                            });
+                            await t;
+                            var v1 = microsoftAPIs.GetAllThings(t.Result.access_token, false);
+                            SquareMinecraftLauncher.Minecraft.Game game = new SquareMinecraftLauncher.Minecraft.Game();
+                            await game.StartGame(versionCombo.Text, javaCombo.Text, Convert.ToInt32(memoryCombo.Text), v1.name, v1.uuid, v1.mcToken, string.Empty, string.Empty);
+                        }
+                        catch (Exception e)
+                        {
+                            MessageBoxX.Show("未知的错误：" + e, "游戏启动失败");
+                        }
                     }
                 }
                 catch(Exception e)
@@ -183,11 +224,11 @@ namespace MCLauncher
         {
             var versions = Core.GetVersions().ToArray();
             versionCombo.ItemsSource = versions;
-                try
-                {
-                    versionCombo.SelectedItem = versionCombo.Items[0];
-                }
-                catch
+            try
+            {
+                versionCombo.SelectedItem = versionCombo.Items[0];
+            }
+            catch
             {
                 MessageBoxX.Show("找不到游戏版本，请重试", "提示");
             }
@@ -203,6 +244,61 @@ namespace MCLauncher
             javaList.Add(tools.GetJavaPath());
             javaCombo.ItemsSource = javaList;
             javaCombo.SelectedItem = javaCombo.Items[0];
+        }
+
+        private void loadconfig_Click(object sender, RoutedEventArgs e)
+        {
+            string path = @"emcl.config";
+            if (File.Exists(path))
+            {
+                try
+                {
+                    string[] contents = File.ReadAllLines(path);
+                    memoryCombo.SelectedItem = contents[0];
+                }
+                catch (Exception ex) {
+                    MessageBoxX.Show(ex.Message, "错误");
+                }
+                try
+                {
+                    string[] contents = File.ReadAllLines(path);
+                    versionCombo.SelectedItem = contents[1];
+                }
+                catch (Exception ex)
+                {
+                    MessageBoxX.Show(ex.Message, "错误");
+                }
+            }
+            else
+            {
+                try
+                {
+                    FileStream newConfig = new FileStream(path, FileMode.Create);
+                    newConfig.Close();
+                }
+                catch (Exception ex)
+                {
+                    MessageBoxX.Show(ex.Message, "错误");
+                }
+            }
+        }
+
+        private void saveconfig_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                var ver = (KMCCC.Launcher.Version)versionCombo.SelectedItem;
+                using (StreamWriter config = new StreamWriter("emcl.config"))
+                {
+                    config.WriteLine(memoryCombo.SelectedItem);
+                    config.WriteLine(ver);
+                    config.Close();
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBoxX.Show(ex.Message, "错误");
+            }
         }
     }
 }
